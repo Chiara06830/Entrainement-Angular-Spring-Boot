d4ca3f879552149378233dfbe1e1e9f2ed7ae164
RELEVAT <chiara.relevat@capgemini.com>
2021-04-09 10:21:39 +0200

    partie spring

---
+++ spring-tour-of-heroes-1/src/main/java/com/example/demo/SpringTourOfHeroes1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTourOfHeroes1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTourOfHeroes1Application.class, args);
+	}
+
+}
